34238c1f2777c303a22aa6842851efa4784b66f3
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-01-07 14:18:59 +0530

    Small change

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use(cors({
     credentials: true
 }))
 
+console.log(process.env.CORS_ORIGIN)
+
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cookieParser())
