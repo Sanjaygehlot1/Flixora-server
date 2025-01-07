a7f29bebfb4df7655b4c4a2ec5ba84efe9e4a81f
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-01-07 10:31:22 +0530

    Small change

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-app.use("/api/v1/users", UserRoute)
+// app.use("/api/v1/users", UserRoute)
 app.post("/api/v1/users/login",(req,res)=>{
     res.send("Login Successfull")
 })
